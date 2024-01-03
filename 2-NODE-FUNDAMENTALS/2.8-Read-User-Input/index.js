7a7be5fbdf0aba10db0efbc09746f825bf8bcd70
Artur <arturrodriguesmota@hotmail.com>
2024-01-03 19:05:21 -0300

    2.8: Read user Input

---
+++ 2-NODE-FUNDAMENTALS/2.8-Read-User-Input/index.js
@@ -0,0 +1,10 @@
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+readline.question("What's your favorite color? ", (color) => {
+
+    console.log(`My favorite color is ${color}`)
+    readline.close()
+})
